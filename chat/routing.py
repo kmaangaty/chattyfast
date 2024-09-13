bd4a2b8005d40c1ac4fc590a5a722346799692e3
Kmangaty <kmaangaty@gmail.com>
2024-09-13 23:56:21 +0300

    add comments and docs for routing

--- chat/routing.py
+++ chat/routing.py
@@ -1,6 +1,8 @@
 from django.urls import re_path
 from . import consumers
 
+# Define WebSocket URL patterns for chat rooms
 websocket_urlpatterns = [
+    # The `re_path` uses a regular expression to capture the room name from the URL
     re_path(r'ws/chat/(?P<room_name>\w+)/$', consumers.ChatConsumer.as_asgi()),
 ]
